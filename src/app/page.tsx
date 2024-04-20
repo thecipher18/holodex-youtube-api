4916ffe4c4e54c8de4ee89390e1bb7577bc27eae
longdo-9801 <61589146+longdo-9801@users.noreply.github.com>
2024-04-20 23:50:26 +0700

    Table V1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,15 @@ export default function Home() {
     <div>
       <div className="text-[red] w-full flex justify-center">navbar</div>
       <div className="text-[red] w-full flex justify-center">body / title</div>
-      <div className="text-[red] w-full flex justify-center">content</div>
+      <div>
+        <h1 className="text-[red] w-full flex justify-center">
+            content
+        </h1>
+        <SongTable/>
+        
+        </div>
       <div className="text-[red] w-full flex justify-center">footer</div>
-      <VTuberInfo name="Kizuna AI" description="Ai vtubeksjhfaskh" />
-      <SongTable nameVideo="１周年記念】ダダダダ天使／湊あくあ【歌ってみた】" views= "20000000" thumbnail= "https://i.ytimg.com/vi/DmURJdzr-Cc/mqdefault.jpg" />
+      
 
       
     </div>
--- src/component/SongTable.tsx
+++ src/component/SongTable.tsx
@@ -3,57 +3,70 @@
 import { ValueOf } from 'next/dist/shared/lib/constants';
 import React, { useState } from 'react'
 
-type SongTableProps = {
-    nameVideo: string,
-    views: string,
-    thumbnail: string,
-}
+// type SongTableProps = {
+//     nameVideo?: string,
+//     views?: number,
+//     thumbnail?: string,
+// }
 
-function SongTable(props: SongTableProps) {
-
-    const { nameVideo, views, thumbnail } = props;
+// function SongTable(props: SongTableProps) {
+function SongTable() {
+    //const { nameVideo, views, thumbnail } = props;
 
     const [number, setNumber] = useState<number>(0);
 
     const jsonData = [
         {
             "nameVideo": "【１周年記念】ダダダダ天使／湊あくあ【歌ってみた】",
-            "views": 20000000,
-            "thumbnail": "https://i.ytimg.com/vi/DmURJdzr-Cc/default.jpg"
+            "views": 500,
+            "thumbnail": "https://i.ytimg.com/vi/DmURJdzr-Cc/default.jpg",
+            "member": "Minato Aqua",
+            "memberThumbnail": "https://yt3.ggpht.com/ytc/AIdro_kaZLtKaya9TSJr3M4lpzV95R2rWdQtGk67fwedroUfSnE=s88-c-k-c0xffffffff-no-rj-mo"
         },
         {
             "nameVideo": "【original】海想列車／湊あくあ【アニメーションMV】",
-            "views": 20000000,
-            "thumbnail": "https://i.ytimg.com/vi/tuWw5EQPGlc/default.jpg"
+            "views": 40000,
+            "thumbnail": "https://i.ytimg.com/vi/tuWw5EQPGlc/default.jpg",
+            "member": "Minato Aqua",
+            "memberThumbnail": "https://yt3.ggpht.com/ytc/AIdro_kaZLtKaya9TSJr3M4lpzV95R2rWdQtGk67fwedroUfSnE=s88-c-k-c0xffffffff-no-rj-mo"
         },
         {
             "nameVideo": "【最終回】ぼっち・ざ・ろっく！同時視聴／BOCCHI THE ROCK!【湊あくあ/ホロライブ】",
-            "views": 20000000,
-            "thumbnail": "https://i.ytimg.com/vi/8mfs-004OAs/default.jpg"
+            "views": 0,
+            "thumbnail": "https://i.ytimg.com/vi/8mfs-004OAs/default.jpg",
+            "member": "Minato Aqua",
+            "memberThumbnail": "https://yt3.ggpht.com/ytc/AIdro_kaZLtKaya9TSJr3M4lpzV95R2rWdQtGk67fwedroUfSnE=s88-c-k-c0xffffffff-no-rj-mo"
         },
     ];
 
+    jsonData.sort((item1,item2)=>item2.views-item1.views);
+
     return (
-        <div className="pl-20">
-        <table className="border-black border-2">
-          <thead className="border-black border-2">
-              <tr>
-                  <td className="border-black border-2">Thumbnail</td>
-                  <td className="border-black border-2">Title</td>
-                  <td className="border-black border-2">Views</td>
-              </tr>
-          </thead>
+        <div className="songTable">
+        <table className='song'>
+            <thead className='song'>
+                <tr>
+                    <td className='song'>Ranking</td>
+                    <td className='song'>Thumbnail</td>
+                    <td className='song'>Title</td>
+                    <td className='song'>Member</td>
+                    <td className='song'>Views</td>
+                </tr>
+            </thead>
             <tbody>
-            {jsonData.map((data) => {
-                return (
-                       
-                            <tr key={data.nameVideo}>
-                                <td className="border-black border-2"><img src={data.thumbnail}></img></td>
-                                <td className="border-black border-2">{data.nameVideo}</td>
-                                <td className="border-black border-2">{data.views}</td>
-                            </tr>
-                       
-                );
+            {jsonData.map((data,index) => {
+                if (index < 30) {
+                    return (
+                        <tr key={data.nameVideo}>
+                            <td className='song'>{index+1}</td>
+                            <td className='song'><img src={data.thumbnail}/></td>
+                            <td className='song'>{data.nameVideo}</td>
+                            <td className='song'><img src={data.memberThumbnail}/><br/>{data.member}</td>
+                            <td className='song'>{data.views}</td>
+                        </tr>
+            );
+                }
+                
             })}
             </tbody>
 
