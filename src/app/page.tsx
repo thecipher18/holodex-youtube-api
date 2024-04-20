1e70c03359da8299543e2be5e801571f362f9c65
flynn_pham <flynn_pham@Flynns-MacBook-Pro.local>
2024-04-19 23:30:35 -0400

    Demo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import VTuberInfo from "@/component/VTuberInfo";
 
 export default function Home() {
   return (
     <div>
-      <div className="text-[red] w-full flex justify-center">change main page</div>
+      <div className="text-[red] w-full flex justify-center">navbar</div>
+      <div className="text-[red] w-full flex justify-center">body / title</div>
+      <div className="text-[red] w-full flex justify-center">content</div>
+      <div className="text-[red] w-full flex justify-center">footer</div>
+      <VTuberInfo name="Kizuna AI" description="Ai vtubeksjhfaskh" />
     </div>
   );
 }
